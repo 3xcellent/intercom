3fedde07274540763bf9e89e30f8d62a4bd25fe3
Jason Gregory <jason.a.gregory@monsanto.com>
2019-06-16 22:14:21 -0500

    do not use code for locking os to main thread yet

--- cmd/client/intercom/intercom.go
+++ cmd/client/intercom/intercom.go
@@ -13,34 +13,6 @@ import (
 	"time"
 )
 
-// Arrange that main.main runs on main thread.
-func init() {
-	runtime.LockOSThread()
-}
-
-// Main runs the main SDL service loop.
-// The binary's main.main must call sdl.Main() to run this loop.
-// Main does not return. If the binary needs to do other work, it
-// must do it in separate goroutines.
-func Main() {
-	for f := range mainfunc {
-		f()
-	}
-}
-
-// queue of work to run in main thread.
-var mainfunc = make(chan func())
-
-// do runs f on the main thread.
-func do(f func()) {
-	done := make(chan bool, 1)
-	mainfunc <- func() {
-		f()
-		done <- true
-	}
-	<-done
-}
-
 const (
 	screenWidth  = 1280 / 2
 	screenHeight = 720 / 2
